e683b4ee194068453fe9e11d2d1364db698ce4fd
jimini <jimini@amazon.com>
2022-10-03 22:49:16 +0900

    initial commit

--- DevSecOps/CodeBuild/scan-image.sh
+++ DevSecOps/CodeBuild/scan-image.sh
@@ -10,7 +10,7 @@ if [ $(echo $?) -eq 0 ]; then
     if [ "$critical" != null ] || [ "$high" != null ]; then
         echo "Docker image contains vulnerabilities at CRITICAL or HIGH level"
         echo $scan_results
-        # 푸시된 이미지가 CRITICAL이나 HIGH 취약점이 있을 경우 삭제하는 절차를 추가 (선택사항)
+        # If the pushed image has CRITICAL or HIGH findings, you may add deleting process. (Optional)
         # aws ecr batch-delete-image --repository-name "$repository_name" --image-ids imageTag="$image_tag"
         # exit 1
       fi
